b3a57429cd50e714f0072480bd16fcec73b794ef
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-22 20:06:41 -0300

    tokenização por sentença usando nltk

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import nltk
 import re
+
+def tokeninzar_sentenca(texto):
+    sentencas = nltk.sent_tokenize(texto)
+    return sentencas
 
 def processamento_texto(textos):
     textos = textos.lower()
@@ -10,4 +15,11 @@ if __name__ == "__main__":
     with open("biblia_limpa.txt", "r", encoding="utf-8") as f:
         biblia_textos = f.read()
 
-    palavras_biblia = processamento_texto(biblia_textos)
+    sentencas_biblia = tokeninzar_sentenca(biblia_textos)
+
+    sentencas_biblias_tokenizadas = []
+
+    for frase in sentencas_biblia[:40]:
+        sentencas_biblias_tokenizadas.append(processamento_texto(frase))
+    
+    print(sentencas_biblias_tokenizadas)
